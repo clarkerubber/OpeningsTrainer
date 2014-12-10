bbbb892df6c34c1cf5aa5fd0a0004242a3b4618d
clarkerubber <jamesjosephclarke@gmail.com>
2014-12-11 09:19:25 +1100

    65K positions to analyse :D

--- convertArray.php
+++ convertArray.php
@@ -1,22 +1,31 @@
 <?
-
-include('openingFens.php');
+ini_set('memory_limit','16000M');
+include('GMOpeningFens.php');
 
 $fenOpenings = array();
-$total = 0;
+$totalFens = 0;
+$fensParsed = 0;
+$gamesParsed = 0;
 
-foreach ($openingFens as $opening => $fens) {
+foreach ($openingFens as $fens) {
+  $opening = array_shift($fens);
+  $gamesParsed++;
   foreach ($fens as $fen) {
+    $fensParsed++;
     $tmp = explode(' ', $fen);
     array_pop($tmp);
     array_pop($tmp);
     $tmp = implode(' ', $tmp);
     if (isset($fenOpenings[$tmp])) {
-      $fenOpenings[$tmp]['openings'][] = $opening;
+      if (!in_array($opening, $fenOpenings[$tmp]['openings'])) {
+        $fenOpenings[$tmp]['openings'][] = $opening;
+      }
+      $fenOpenings[$tmp]['nb']++;
     } else {
-      $total++;
+      $totalFens++;
       $fenOpenings[$tmp]['fen'] = $fen;
       $fenOpenings[$tmp]['openings'][0] = $opening;
+      $fenOpenings[$tmp]['nb'] = 1;
     }
   }
 }
@@ -24,7 +33,10 @@ foreach ($openingFens as $opening => $fens) {
 $output = array();
 
 foreach ($fenOpenings as $key => $value) {
-  $output[] = $value;
+  if ($value['nb'] > 14) {
+    $output[] = $value;
+  }
 }
-
+//echo count($openingFens)."\n";
+//echo "Games Parsed: $gamesParsed\nFENs Parsed: $fensParsed\nTotal FENs: $totalFens\n";
 print_r(json_encode($output, JSON_PRETTY_PRINT));
