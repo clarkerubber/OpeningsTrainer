991873dfaa1cc2fb26a411af4ae8bc267154706b
clarkerubber <jamesjosephclarke@gmail.com>
2014-12-16 19:31:47 +1100

    Increase second pass time

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ $BALANCED = 150; // +-centipawns for a position to be considered even
 //-----engine settings
 $STOCKFISH_PATH = "/Users/clarkey/Documents/Development/lichess/stockfish-5-mac/Mac/stockfish-5-64"; // location of stockfish engine
 $FIRST_PASS_TIME = 10000; // milliseconds to gather candidate moves
-$SECOND_PASS_TIME = 10000; // milliseconds to consider each candidate move
+$SECOND_PASS_TIME = 15000; // milliseconds to consider each candidate move
 
 $MULTIPV = 15;
